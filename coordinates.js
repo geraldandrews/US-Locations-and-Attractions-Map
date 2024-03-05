370f13e22890b723b0a9f7cbae5f8048b116a78b
Gerald Andrews <gerald2399@comcast.net>
2024-03-05 10:20:55 -0500

    Initial commit

---
+++ coordinates.js
@@ -0,0 +1,17 @@
+// Cities as a JavaScript object
+
+var cities = {
+	"type": "FeatureCollection",
+	"features": [{
+		"geometry": {
+			"type": "Point",
+			"coordinates": [123, 135]
+		},
+		"type": "Feature",
+		"properties": {
+			"wikipedia": "Boston",
+			"city": "Boston, Massachusetts"
+		},
+		"id": "Boston"
+    }]
+}
